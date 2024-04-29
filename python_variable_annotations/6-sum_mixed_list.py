e0a2749ed1d281af74b82c2b679a91cbce3b6388
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-04-29 16:50:37 +0400

    6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+task 6
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """doc func"""
+    return sum(mxd_lst)
