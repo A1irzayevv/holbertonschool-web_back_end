4b17f961cace2353aa6a3ad54b7988d83bd2aa5b
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-05-05 12:07:22 +0400

    1 pyc fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """doc func"""
-    dly  = []
+    dly = []
     for i in range(n):
         dly.append(await wait_random(max_delay))
     return sorted(dly)
