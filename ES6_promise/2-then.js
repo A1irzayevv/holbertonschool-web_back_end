970b8fe5614784ab3f4678c29bd3158a4bbffe12
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-05-24 15:50:28 +0400

    2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,17 +1,10 @@
 function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
   return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      const error = Promise.reject(new Error());
-      return error;
-    });
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
